a74bf6c5bf61a9390b943ea88ccc9a2b80c6db0e
AshTech-Development <ashtechdevelopment0@gmail.com>
2024-06-25 17:05:13 -0400

    eso_addon_data.py

--- eso_addon_data.py
+++ eso_addon_data.py
@@ -1,5 +1,9 @@
 # eso_addon_data.py
 from flask import request, jsonify
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 eso_addon_data = {}
 
@@ -7,9 +11,12 @@ def receive_eso_addon_data():
     data = request.get_json()
     if data:
         eso_addon_data.update(data)
+        logger.debug("Received data: %s", data)  # Log the received data
         return jsonify({'message': 'Data received successfully'}), 200
     else:
+        logger.debug("No data received")  # Log when no data is received
         return jsonify({'error': 'No data received'}), 400
 
 def get_eso_addon_data():
+    logger.debug("Retrieved data: %s", eso_addon_data)  # Log the retrieved data
     return eso_addon_data
